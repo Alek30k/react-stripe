ba132292641c9d709e1b84f680dc846bcb462d90
Alek30k <alemank23k@gmail.com>
2022-09-16 14:41:36 -0300

    last commit

--- src/Pay.jsx
+++ src/Pay.jsx
@@ -1,6 +1,36 @@
 import StripeCheckout from "react-stripe-checkout";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const KEY =
+  "pk_test_51LWR1rCTlhsOX9GfhjESQ1bu9wYMwpoNoxZcSF3aR0QQAzzsH2uyewThu2I4hy1oFWGfq48N1jkTSb65or0fvC0900JuYognmI";
 
 const Pay = () => {
+  const [stripeToken, setStripeToken] = useState(null);
+
+  const onToken = (token) => {
+    setStripeToken(token);
+    // console.log(token);
+  };
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await axios.post(
+          "http://localhost:5000/api/checkout/payment",
+          {
+            tokenId: stripeToken.id,
+            amount: 2000,
+          }
+        );
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    stripeToken && makeRequest();
+  }, [stripeToken]);
+
   return (
     <div
       style={{
@@ -10,22 +40,35 @@ const Pay = () => {
         justifyContent: "center",
       }}
     >
-      <StripeCheckout>
-        <button
-          style={{
-            border: "none",
-            with: 120,
-            borderRadius: 5,
-            padding: "20px",
-            backgroundColor: "black",
-            color: "white",
-            fontWeight: "600",
-            cursor: "pointer",
-          }}
+      {stripeToken ? (
+        <span>Prcessing. Please wait...</span>
+      ) : (
+        <StripeCheckout
+          name="ArC shop"
+          image="https://user-images.githubusercontent.com/101005998/190654889-f7553f5a-f702-4208-9a2b-760ea5b3b381.jpg"
+          billingAddress
+          shippingAddress
+          description="Your total is $20"
+          amount={2000}
+          token={onToken}
+          stripeKey={KEY}
         >
-          Pay Now
-        </button>
-      </StripeCheckout>
+          <button
+            style={{
+              border: "none",
+              with: 120,
+              borderRadius: 5,
+              padding: "20px",
+              backgroundColor: "black",
+              color: "white",
+              fontWeight: "600",
+              cursor: "pointer",
+            }}
+          >
+            Pay Now
+          </button>
+        </StripeCheckout>
+      )}
     </div>
   );
 };
